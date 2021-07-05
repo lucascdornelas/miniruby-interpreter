ea37af05e6531fef5c63284b45484bd9e90b68e9
ErickHDdS <erickhenriquedds@gmail.com>
2021-07-05 20:40:46 -0300

    FEAT: AccessExpr and BinaryExpr

---
+++ miniRuby/JAVA/interpreter/expr/AccessExpr.java
@@ -0,0 +1,103 @@
+package interpreter.expr;
+
+import java.util.Vector;
+
+import interpreter.util.Abort;
+import interpreter.value.ArrayValue;
+import interpreter.value.IntegerValue;
+import interpreter.value.StringValue;
+import interpreter.value.Value;
+
+public class AccessExpr extends SetExpr{
+    private Expr base;
+    private Expr index;
+
+    public AccessExpr(int line, Expr base, Expr index) {
+        super(line);
+        this.base = base;
+        this.index = index;
+    }
+
+    @Override
+    public Value<?> expr() {
+        Value<?> value = base.expr();
+
+        if(this.index == null) {
+            Abort.abort(super.getLine());
+        }
+        else {
+            if(value instanceof ArrayValue) {
+                Value<?> index_aux = this.index.expr();
+                int indexVector = 0;
+
+                ArrayValue arrayValue = (ArrayValue) value;
+                Vector<Value<?>> vector = arrayValue.value();
+
+                if(!(index_aux instanceof ArrayValue)) {
+                    indexVector = Integer.parseInt(index_aux.toString());
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+
+                return vector.get(indexVector);
+            }
+            else {
+                Abort.abort(super.getLine());
+            }
+        }
+
+        return value;
+    }
+
+    @Override
+    public void setValue(Value<?> value) {
+        Value<?> v = this.base.expr();
+        SetExpr sexpr = (SetExpr) base;
+
+        if(this.index == null) {
+            if(value instanceof StringValue) {
+                StringValue stringValue = (StringValue) value;
+                String string = stringValue.value();
+                StringValue stringValue_new = new StringValue(string);
+                sexpr.setValue(stringValue_new);
+            }
+            else {
+                if(value instanceof IntegerValue) {
+                    IntegerValue integerValue = (IntegerValue) value;
+                    int number = integerValue.value();
+                    IntegerValue integerValue_new = new IntegerValue(number);
+                    sexpr.setValue(integerValue_new);
+                }
+                else {
+                    sexpr.setValue(value);
+                }
+            }
+        }
+        else {
+            if(v instanceof ArrayValue) {
+                Value<?> index_aux = this.index.expr();
+                int indexVector = 0;
+                ArrayValue arrayValue = (ArrayValue) v;
+                Vector<Value<?>> vector = arrayValue.value();
+
+                if(!(index_aux instanceof ArrayValue)) {
+                    indexVector = Integer.parseInt(index_aux.toString());
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+
+                vector.set(indexVector, value);
+                ArrayValue arrayValue_new = new ArrayValue(vector);
+
+                sexpr.setValue(arrayValue_new);
+
+            }
+            else {
+                Abort.abort(super.getLine());
+            }
+        }
+    }
+
+}
---
+++ miniRuby/JAVA/interpreter/expr/BinaryExpr.java
@@ -0,0 +1,164 @@
+package interpreter.expr;
+
+import java.util.Vector;
+
+import interpreter.util.Abort;
+import interpreter.value.ArrayValue;
+import interpreter.value.IntegerValue;
+import interpreter.value.StringValue;
+import interpreter.value.Value;
+
+public class BinaryExpr extends Expr{
+    private Expr left;
+    private BinaryOp op;
+    private Expr right;
+    private int result;
+
+    public BinaryExpr (int line, Expr left, BinaryOp op, Expr right) {
+        super(line);
+        this.left = left;
+        this.op = op;
+        this.right = right;
+        this.result = 0;
+    }
+
+    @Override
+    public Value<?> expr() {
+        Value<?> value = null;
+        Value<?> left = this.left.expr();
+        Value<?> right = this.right.expr();
+
+        int leftValue = Integer.parseInt(left.toString());
+        int rightValue = Integer.parseInt(right.toString());
+
+        int result;
+
+        switch(this.op) {
+            case RangeWithOp:
+                if(left instanceof IntegerValue && right instanceof IntegerValue) {
+                    Vector<Value<?>> resultVec = new Vector<Value<?>>();
+
+                    for(int count = leftValue; count <= rightValue+1; count ++) {
+                        IntegerValue integerValue_new = new IntegerValue(count);
+                        resultVec.add(integerValue_new);
+                    }
+
+                    ArrayValue arrayValue_new = new ArrayValue(resultVec);
+                    value = arrayValue_new;
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+                break;
+
+            case RangeWithoutOp:
+                if(left instanceof IntegerValue && right instanceof IntegerValue) {
+                    Vector<Value<?>> resultVec = new Vector<Value<?>>();
+
+                    for(int count = leftValue; count < rightValue+1; count ++) {
+                        IntegerValue integerValue_new = new IntegerValue(count);
+                        resultVec.add(integerValue_new);
+                    }
+
+                    ArrayValue arrayValue_new = new ArrayValue(resultVec);
+                    value = arrayValue_new;
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+                break;
+
+            case AddOp:
+                if(left instanceof IntegerValue && right instanceof IntegerValue) {
+                    result = leftValue + rightValue;
+
+                    IntegerValue integerValue_new = new IntegerValue(result);
+                    value = integerValue_new;
+                }
+                else {
+                    if(left instanceof ArrayValue && right instanceof ArrayValue) {
+                        Vector<Value<?>> resultVec = new Vector<Value<?>>();
+
+                        ArrayValue leftValueArray = (ArrayValue) left;
+                        ArrayValue rightValueArray = (ArrayValue) right;
+                        Vector<Value<?> > leftVector = leftValueArray.value();
+                        Vector<Value<?> > rightVector = rightValueArray.value();
+
+                        resultVec.addAll(leftVector);
+                        resultVec.addAll(rightVector);
+
+                        ArrayValue arrayValue_new = new ArrayValue(resultVec);
+                        value = arrayValue_new;
+                    }
+                    else {
+                        if(left instanceof StringValue && right instanceof StringValue) {
+                            String leftValueString = left.toString();
+                            leftValueString = leftValueString.replace("\'", "");
+                            String rightValueString = right.toString();
+                            rightValueString = rightValueString.replace("\'", "");
+                            String resultStr = leftValueString + rightValueString;
+                            String apostrophe = "\'";
+                            resultStr = apostrophe + resultStr + apostrophe;
+                            StringValue stringValue_new = new StringValue (resultStr);
+                            value = stringValue_new;
+                        }
+                        else {
+                            Abort.abort(super.getLine());
+                        }
+                        break;
+                    }
+                }
+
+            case SubOp:
+                if(left instanceof IntegerValue && right instanceof IntegerValue) {
+                    this.result = leftValue - rightValue;
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+                break;
+
+            case MulOp:
+                if(left instanceof IntegerValue && right instanceof IntegerValue) {
+                    this.result = leftValue * rightValue;
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+                break;
+
+            case DivOp:
+                if(left instanceof IntegerValue && right instanceof IntegerValue) {
+                    this.result = leftValue / rightValue;
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+                break;
+
+            case ModOp:
+                if(left instanceof IntegerValue && right instanceof IntegerValue) {
+                    this.result = leftValue % rightValue;
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+                break;
+
+            case ExpOp:
+                if(left instanceof IntegerValue && right instanceof IntegerValue) {
+                    this.result = (int) Math.pow(leftValue,rightValue);
+                }
+                else {
+                    Abort.abort(super.getLine());
+                }
+                break;
+        }
+
+        if(this.op == BinaryOp.SubOp || this.op == BinaryOp.MulOp  || this.op == BinaryOp.DivOp || this.op == BinaryOp.ModOp || this.op == BinaryOp.ExpOp ) {
+            IntegerValue integerValue_new = new IntegerValue(this.result);
+            value = integerValue_new;
+        }
+        return value;
+    }
+}
