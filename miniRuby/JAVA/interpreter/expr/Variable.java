5a78d888e07e45bb84de387da428d014ade3ba0c
ErickHDdS <erickhenriquedds@gmail.com>
2021-07-05 18:42:28 -0300

    FEAT: Variable and SetExpr

---
+++ miniRuby/JAVA/interpreter/expr/Variable.java
@@ -0,0 +1,28 @@
+package interpreter.expr;
+
+import interpreter.util.Memory;
+import interpreter.value.Value;
+
+public class Variable extends SetExpr{
+    private String name;
+
+    public Variable(int line, String name){
+        super(line);
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public Value<?> expr() {
+        return Memory.read(name);
+    }
+
+    @Override
+    public void setValue(Value<?> value) {
+        Memory.write(name, value);
+    }
+
+}
