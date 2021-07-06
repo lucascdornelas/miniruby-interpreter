20a08e7c18a98d26f4a77e2ca1aca22557fa3670
Lucas Cota <lucascdornelas@gmail.com>
2021-07-06 18:12:22 -0300

    fix: add method execute of class ForCommand

--- miniRuby/JAVA/interpreter/command/ForCommand.java
+++ miniRuby/JAVA/interpreter/command/ForCommand.java
@@ -1,30 +1,53 @@
 package interpreter.command;
 
+import java.util.Vector;
+
 import interpreter.expr.Expr;
+import interpreter.expr.Variable;
+import interpreter.util.Exit;
+import interpreter.value.ArrayValue;
+import interpreter.value.IntegerValue;
+import interpreter.value.Value;
 
 public class ForCommand extends Command{
     private Variable var;
-    private Expr expt;
+    private Expr expr;
     private Command cmds; 
 
     public ForCommand(int line, Variable var, Expr expr, Command cmds) {
         super(line);
 
         this.var = var;
-        this.expt = expr;
+        this.expr = expr;
         this.cmds = cmds;
     }
 
     @Override
     public void execute() {
-        this.var.setValue(this.expt.expr());
+        Value<?> value = this.expr.expr();
 
-        int j = Integer.parseInt(this.expt.expr().value());
-
-        for(int i = var; i < j; i++){
-            cmds.execute();
-
-            var.setValue(i);
+        if (value instanceof IntegerValue) {            
+            var.setValue(new IntegerValue(0));
+            int i = Integer.parseInt(var.expr().toString());
+            int iv = Integer.parseInt(value.toString());
+            while (i < iv) {
+                cmds.execute();
+                i++;
+                var.setValue(new IntegerValue(i));
+            }
+        } else if (value instanceof ArrayValue) {                       
+            ArrayValue av = (ArrayValue) value;
+            Vector<Value<?>> vec = av.value();
+            int i = Integer.parseInt(vec.firstElement().toString());
+            var.setValue(new IntegerValue(i));
+            int last = Integer.parseInt(vec.lastElement().toString());              
+            while (i < last) {
+                cmds.execute();
+                i++;
+                var.setValue(new IntegerValue(i));
+            }            
+        } else {
+            Exit.exit(super.getLine());
         }
     }
 }
